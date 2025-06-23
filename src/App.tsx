4129152b0ddfa3e604774adc7746b28f67cbef46
oguzhan <ogzzcelik@gmail.com>
2025-06-23 17:22:49 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+       <Button variant={'destructive'}>Click me</Button>
+    </>
+  )
+}
+
+export default App

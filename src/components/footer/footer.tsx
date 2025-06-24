4d26ca54d09e7d6ab5c3b58927bc59cd54e02e6d
oguzhanceliko <ogzzcelik@gmail.com>
2025-06-24 10:14:30 +0300

    test

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,6 +5,7 @@ const Footer = () => {
 
   return (
     <footer className="bg-gray-100 dark:bg-gray-800  dark:border-t dark:border-gray-700">
+
       <div className="container mx-auto px-4 py-6">
         <div className="text-center">
           <p>&copy; 2025 {t("footer.copyright")}</p>
